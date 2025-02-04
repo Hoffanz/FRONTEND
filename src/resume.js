fe1c49d12a0e2632ba81e7e548de8feb2a6f3f16
Jonathan <jonathanfromsweden@gmail.com>
2025-02-04 12:31:14 +0100

    Overhaul, removed TONS of stuff

--- src/resume.js
+++ src/resume.js
@@ -1,6 +1,6 @@
 document.getElementById("cv-section").addEventListener("click", () => {
   const link = document.createElement("a");
-  link.href = "docs/CV-2024.pdf";
+  link.href = "/FRONTEND/CV-2024.pdf";
   link.download = "CV-2024.pdf";
   link.click();
 });
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base:"/portfolio-vite/",
-    build: {
-        outDir: "dist"
-    },
+    base:"/FRONTEND/",
 });
